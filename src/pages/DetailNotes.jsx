49ff462b11c65e4ed7e49a6702033e1724fa2ef1
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2023-04-30 15:52:27 +0700

    commit dulu

--- src/pages/DetailNotes.jsx
+++ src/pages/DetailNotes.jsx
@@ -3,20 +3,20 @@ import { useParams } from 'react-router-dom';
 import { showFormattedDate } from '../utils/index';
 
 const DetailNotes = ({ notes }) => {
-    const { id } = useParams();
-    const note = notes.find((note) => note.id === parseInt(id));
-  
-    return note ? (
-      <div className="note-detail">
-        <h2 className="note-detail__title">{note.title}</h2>
-        <p className="note-detail__content">{note.body}</p>
-        <p className="note-detail__date">{showFormattedDate(note.dateCreated)}</p>
-      </div>
-    ) : (
-      <div className="note-detail">
-        <p>Note not found</p>
-      </div>
-    );
+  const { id } = useParams();
+  const note = notes.find((note) => note.id === parseInt(id));
+
+  return note ? (
+    <div className="note-detail">
+      <h2 className="note-detail__title">{note.title}</h2>
+      <p className="note-detail__content">{note.body}</p>
+      <p className="note-detail__date">{showFormattedDate(note.dateCreated)}</p>
+    </div>
+  ) : (
+    <div className="note-detail">
+      <p>Note not found</p>
+    </div>
+  );
 };
 
 export default DetailNotes;
